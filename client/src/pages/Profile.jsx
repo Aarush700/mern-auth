a4f662165d87fdef0b9506b6559084f74f167342
Aarush700 <aarush.singh@live.com>
2025-09-28 12:44:39 +1000

    Fixes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,25 @@
-import React from "react";
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
 
 function Profile() {
+    const fileRef = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
 
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
             <form className="flex flex-col gap-4">
+                <input
+                    type="file"
+                    ref={fileRef}
+                    hidden
+                    accept="image/*"
+                />
                 <img
                     src={currentUser.profilePicture}
                     alt="Profile"
                     className="mt-2 h-24 w-24 self-center cursor-pointer rounded-full object-cover border-2"
+                    onClick={() => fileRef.current.click()}
                 />
                 <input
                     type="text"
